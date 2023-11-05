20a3cac8b613688461cb49666f10563a48557619
Shedshyu <90133181+NurikJK@users.noreply.github.com>
2023-11-05 11:34:02 +0600

    Chapter 10 done

--- hw/assignment_2/cmd/api/routes.go
+++ hw/assignment_2/cmd/api/routes.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
 	router.HandlerFunc(http.MethodPost, "/v1/products", app.createProductHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/products/:id", app.showProductHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/products", app.listProductsHandler)
@@ -19,5 +20,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/products/:id", app.updateProductHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/products/:id", app.deleteProductHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
